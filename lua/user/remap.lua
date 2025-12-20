e4a3a96b0d9069f744290ddf882a807afb70fcef
Stefan Berger <berger.st.11.11@gmail.com>
2025-12-20 16:41:06 +0100

    reformat on save

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -23,12 +23,7 @@ local function build_and_run_current_day()
 	-- Assume the executable lives at build/<day_dir>/<day_dir>
 	-- Adjust this mapping if your target name differs from the directory name.
 	local target_name = day_dir
-	local cmd = string.format(
-		"cd %s && cmake --build build && ./build/%s/%s",
-		project_root,
-		day_dir,
-		target_name
-	)
+	local cmd = string.format("cd %s && cmake --build build && ./build/%s/%s", project_root, day_dir, target_name)
 
 	vim.cmd("!" .. cmd)
 end
@@ -45,7 +40,6 @@ map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
 map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
 map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
 
-
 map("v", "J", ":m '>+1<CR>gv=gv", { desc = "move selection down" })
 map("v", "K", ":m '<-2<CR>gv=gv", { desc = "move selection up" })
 
@@ -55,7 +49,7 @@ map("n", "n", "nzzzv", { desc = "next search centered" })
 map("n", "N", "Nzzzv", { desc = "previous search centered" })
 
 -- build & run CMake target
-map("n", "<leader>r", build_and_run_current_day, { desc = "Build and run current day" })
+map("n", "<leader>dr", build_and_run_current_day, { desc = "Build and run current day" })
 
 -- nvimtree
 map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
@@ -65,15 +59,33 @@ map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle windo
 map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "LSP diagnostic loclist" })
 
 -- lsp keybindings (on_attach)
-map("n", "gd", function() vim.lsp.buf.definition() end, { desc = "Go to definition" })
-map("n", "K", function() vim.lsp.buf.hover() end, { desc = "Hover documentation" })
-map("n", "gr", function() vim.lsp.buf.references() end, { desc = "Show references" })
-map("n", "<leader>rn", function() vim.lsp.buf.rename() end, { desc = "Rename symbol" })
-map("n", "<leader>ca", function() vim.lsp.buf.code_action() end, { desc = "Code action" })
-map("n", "[d", function() vim.diagnostic.goto_prev() end, { desc = "Previous diagnostic" })
-map("n", "]d", function() vim.diagnostic.goto_next() end, { desc = "Next diagnostic" })
-map("n", "<leader>d", function() vim.diagnostic.open_float() end, { desc = "Open diagnostic float" })
-map("n", "<leader>f", function() vim.lsp.buf.format({ async = true }) end, { desc = "Format buffer" })
+map("n", "gd", function()
+	vim.lsp.buf.definition()
+end, { desc = "Go to definition" })
+map("n", "K", function()
+	vim.lsp.buf.hover()
+end, { desc = "Hover documentation" })
+map("n", "gr", function()
+	vim.lsp.buf.references()
+end, { desc = "Show references" })
+map("n", "<leader>rn", function()
+	vim.lsp.buf.rename()
+end, { desc = "Rename symbol" })
+map("n", "<leader>ca", function()
+	vim.lsp.buf.code_action()
+end, { desc = "Code action" })
+map("n", "[d", function()
+	vim.diagnostic.goto_prev()
+end, { desc = "Previous diagnostic" })
+map("n", "]d", function()
+	vim.diagnostic.goto_next()
+end, { desc = "Next diagnostic" })
+map("n", "<leader>d", function()
+	vim.diagnostic.open_float()
+end, { desc = "Open diagnostic float" })
+map("n", "<leader>f", function()
+	vim.lsp.buf.format({ async = true })
+end, { desc = "Format buffer" })
 
 -- Comment
 map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
@@ -93,8 +105,13 @@ map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git
 map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
 map("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidden term" })
 map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "telescope find files" })
-map("n","<leader>fa","<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",{ desc = "telescope find all files" })
+map(
+	"n",
+	"<leader>fa",
+	"<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
+	{ desc = "telescope find all files" }
+)
 
 -- remap brackets to german layout
-map("n", "ü", "[", {noremap=true})
-map("n", "ä", "]", {noremap=true})
+map("n", "ü", "[", { noremap = true })
+map("n", "ä", "]", { noremap = true })
